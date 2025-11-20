2fa767bf4d13e8d307a0518e427f5601325fbcf8
Yenuli Minsadi <samararatney@gmail.com>
2025-11-20 10:24:20 +0530

    avoid cross policy conflict using ajax

--- JAVAEE/JAVAEE_Theory/Day_01/src/HelloServlet.java
+++ JAVAEE/JAVAEE_Theory/Day_01/src/HelloServlet.java
@@ -7,7 +7,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/saved")
+//API Endpoint, mapping to servlet - request, responses
+@WebServlet(urlPatterns="/saved")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
