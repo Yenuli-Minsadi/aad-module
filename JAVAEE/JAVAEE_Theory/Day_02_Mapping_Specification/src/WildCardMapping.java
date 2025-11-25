61e3fe4c379a94aa665dc26cf1e2766acfe77d92
Yenuli Minsadi <samararatney@gmail.com>
2025-11-25 19:35:41 +0530

    update

--- JAVAEE/JAVAEE_Theory/Day_02_Mapping_Specification/src/WildCardMapping.java
+++ JAVAEE/JAVAEE_Theory/Day_02_Mapping_Specification/src/WildCardMapping.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/auth")
+@WebServlet(urlPatterns = "/auth/*")
 public class WildCardMapping extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
