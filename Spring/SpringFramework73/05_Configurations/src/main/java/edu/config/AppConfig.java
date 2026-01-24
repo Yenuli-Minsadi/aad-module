76a04458f02c313b9a57b83b694a876d5b284d56
Yenuli Minsadi <samararatney@gmail.com>
2026-01-24 13:53:37 +0530

    update with importing xml files to project using @ImportResource annotation

--- Spring/SpringFramework73/05_Configurations/src/main/java/edu/config/AppConfig.java
+++ Spring/SpringFramework73/05_Configurations/src/main/java/edu/config/AppConfig.java
@@ -3,10 +3,13 @@ package edu.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Import({AppConfig1.class, AppConfig2.class})
 @Configuration
 @ComponentScan(basePackages = "edu.bean")
+@ImportResource("classpath:hibernate.cfg.xml")//to import xml files in our project, relative path, inna class eke folder structure eken access krnne
+//@ImportResource("file:C:folder name")  - in absolute path/hard drive/os path
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig obj created");
