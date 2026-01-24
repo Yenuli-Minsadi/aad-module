d64808b53af06245da3d400792b9d1986c01a0eb
Yenuli Minsadi <samararatney@gmail.com>
2026-01-24 14:20:21 +0530

    update corrected

--- Spring/SpringFramework73/06_Runtime_Value_Injection/src/main/java/edu/bean/SpringBean.java
+++ Spring/SpringFramework73/06_Runtime_Value_Injection/src/main/java/edu/bean/SpringBean.java
@@ -6,12 +6,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
-    @Value("Yen")
-    private String name;
+   
     public SpringBean(@Value("Yen")String name) {
         System.out.println(name);//obj ek create weddi value set wenne na acc to lifecycle
     }
-    
+
 
 //    @Override
 //    public void afterPropertiesSet() throws Exception {
