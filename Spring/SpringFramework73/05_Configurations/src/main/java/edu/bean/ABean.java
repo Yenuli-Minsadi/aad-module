2fe2a9d1ece65fbaab4ed6227441f1b9a16582d5
Yenuli Minsadi <samararatney@gmail.com>
2026-01-24 13:30:07 +0530

    update

---
+++ Spring/SpringFramework73/05_Configurations/src/main/java/edu/bean/ABean.java
@@ -0,0 +1,10 @@
+package edu.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ABean {
+    public ABean() {
+        System.out.println("ABean obj created");
+    }
+}
--- Spring/SpringFramework73/05_Configurations/src/main/java/edu/config/AppConfig1.java
+++ Spring/SpringFramework73/05_Configurations/src/main/java/edu/config/AppConfig1.java
@@ -1,12 +1,18 @@
 package edu.config;
 
+import edu.bean.ABean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "edu.bean")
 public class AppConfig1 {
     public AppConfig1() {
         System.out.println("AppConfig1 obj created");
+    }
+
+    @Bean
+    public ABean aBean() {
+        return new ABean();
     }
 }
