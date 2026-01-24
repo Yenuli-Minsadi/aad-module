9b68f8a626923a5406c2dd22ff6c972b1da174fa
Yenuli Minsadi <samararatney@gmail.com>
2026-01-24 14:37:18 +0530

    update to set values at runtime through constructor and check runtime execution with no. of paras

--- Spring/SpringFramework73/06_Runtime_Value_Injection/src/main/java/edu/bean/SpringBean.java
+++ Spring/SpringFramework73/06_Runtime_Value_Injection/src/main/java/edu/bean/SpringBean.java
@@ -1,16 +1,25 @@
 package edu.bean;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
-   
-    public SpringBean(@Value("Yen")String name) {
-        System.out.println(name);//obj ek create weddi value set wenne na acc to lifecycle
+
+    @Autowired(required = false)
+    public SpringBean(@Value("Yen")String nameList[], @Value("12")int number) {
+        System.out.println(nameList.length);//obj ek create weddi value set wenne na acc to lifecycle
+        System.out.println(number);
+//        System.out.println(address);
     }
 
+    @Autowired(required = false)
+    public SpringBean(@Value("Yen")String name, @Value("22") int age) {
+        System.out.println(name);//obj ek create weddi value set wenne na acc to lifecycle
+        System.out.println(age);
+    }
 
 //    @Override
 //    public void afterPropertiesSet() throws Exception {
